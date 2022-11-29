94cf964d202fea02c281a5e84458ab612d173195
paramSingh1 <paramdeep2009@hotmail.com>
2022-11-29 19:08:03 +1100

    render preview file

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
+import { clock } from "./clock.js";
+import { renderItems } from "./renderItems.js";
 const startBtn = document.querySelector(".start");
 const menuDisplay = document.querySelector(".startMenu__modal");
 
@@ -19,108 +21,7 @@ desktop.addEventListener("click", () => {
   }
 });
 
-console.log(startBtn);
 const windowBody = document.querySelector(".window__container__body");
-
-const renderItems = (id) => {
-  clearWindow();
-
-  switch (id) {
-    case "bin":
-      console.log("bin", id);
-
-      customElement(windowBody, "div", "", explorerDiv);
-
-      console.log("ok");
-      break;
-
-    case "ie":
-      customElement(
-        windowBody,
-        "iframe",
-        "https://askjeeves.net/",
-        "windowIframe"
-      );
-      break;
-    case "mydocs":
-      console.log("hi", id);
-      break;
-
-    case "notepad":
-      console.log("clicked on", id);
-      customElement(windowBody, "textArea", "", "notepad__TextArea");
-      const textArea = document.querySelector(".notepad__TextArea");
-      textArea.style.width = "100%";
-      textArea.style.height = "100%";
-      textArea.style.resize = "none";
-
-      break;
-    case "resume":
-      customElement(
-        windowBody,
-        "iframe",
-        "https://www.linkedin.com/in/paramsinghau/",
-        "windowIframe"
-      );
-      break;
-    case "github":
-      customElement(
-        windowBody,
-        "iframe",
-        "https://github.com/paramSingh1",
-        "windowIframe"
-      );
-      break;
-    case "minesweeper":
-      customElement(
-        windowBody,
-        "iframe",
-        "https://minesweeper.online/",
-        "windowIframe"
-      );
-      break;
-    default:
-      console.log(id);
-  }
-};
-const customElement = (parent, eleType, srcLink, className) => {
-  console.log(parent);
-  const newElement = document.createElement(`${eleType}`);
-  console.log(newElement);
-  newElement.src = "";
-  newElement.src = srcLink;
-  newElement.classList.add(`${className}`);
-  parent.appendChild(newElement);
-};
-const clearWindow = () => {
-  windowBody.innerHTML = "";
-};
-const clock = () => {
-  const target = document.getElementById("clock");
-  // console.log(target.innerHTML);
-
-  let date = new Date();
-  let hours = date.getHours();
-  let minutes = date.getMinutes();
-  let meridian = "AM";
-
-  //convert time to 12 hour format
-  if (hours == 0) {
-    hours = 12;
-  } else if (hours > 12) {
-    hours = hours - 12;
-    meridian = "PM";
-  }
-
-  hours = hours < 10 ? "0" + hours : hours;
-  minutes = minutes < 10 ? "0" + minutes : minutes;
-
-  let time = `${hours}:${minutes} ${meridian} `;
-  // CHANGE THIS VALUE BACK TO 1000
-  setInterval(clock, 1000);
-
-  target.innerText = time;
-};
 
 //Add close button event listener.
 // turn this into a function
@@ -130,7 +31,6 @@ const taskbarProgram = document.querySelector(".openProgram__container");
 
 closeBtn.addEventListener("click", () => {
   if (windowContainer.style.display === "flex") {
-    console.log(windowContainer.style.display);
     windowContainer.style.display = "none";
     taskbarProgram.style.display = "none";
   } else {
@@ -173,7 +73,6 @@ desktopIcons.forEach((ele) =>
 );
 
 const removeIconBorders = () => {
-  console.log(desktopIcons);
   desktopIcons.forEach((ele) => {
     ele.style.border = "0px solid white";
   });
